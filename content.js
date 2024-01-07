48653c4cd31dd770d8f519870b939d606ed03e9e
Ollie Smith <o.g.smith@icloud.com>
2024-01-07 20:07:56 +0000

    Block the ads faster and remove duplicate code

--- content.js
+++ content.js
@@ -1,55 +1,4 @@
 function removeAds() {
-
-    console.log("ITVX Adblocker Active")
-
-    elem = document.getElementsByClassName("genie-container")[0]
-
-    if (elem) {
-
-        attrib = elem.getAttribute("data-video-id")
-
-        if (!attrib) {
-
-            console.log("Video ID not found");
-            return;
-        }
-
-        rawId = attrib.split("/").slice(-1)[0];
-        idBase = rawId.split(".")[0].replaceAll("_", "/");
-        idIndex = rawId.split(".")[1];
-
-        if (!idBase || !idIndex) {
-
-            console.log("Video ID in unknown format");
-            return;
-        }
-
-        console.log("Episode IDs:", rawId, idBase, idIndex);
-
-        timestamp = Date.now() - 60000
-        breaksWatched = {
-            indexes: [...Array(20).keys()],
-            timestamp: timestamp
-        }
-
-        storage = JSON.parse(localStorage.getItem("genie-productions")) || {};
-        storage[idBase] = storage[idBase] || {};
-        storage[idBase][idIndex] = storage[idBase][idIndex] || {};
-        storage[idBase][idIndex].breaksWatched = breaksWatched;
-
-        localStorage.setItem("genie-productions", JSON.stringify(storage));
-
-        console.log("Removed ad breaks 1-20")
-
-    } else {
-
-        console.log("Video element not found");
-    }
-}
-
-removeAds();
-
-function removeAds() {
     console.log("ITVX Adblocker Active");
 
     function tryRemovingAds(attempt) {
@@ -94,7 +43,7 @@ function removeAds() {
         } else {
             if (attempt < 5) {
                 console.log("Video element not found, retrying...");
-                setTimeout(() => tryRemovingAds(attempt + 1), 1000);
+                setTimeout(() => tryRemovingAds(attempt + 1), 500);
             } else {
                 console.log("Failed to find video element after 5 attempts");
             }
